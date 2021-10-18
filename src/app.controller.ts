d3e13e12fce4c61d538ea91ab62608c748db34d8
greatleee <cd.lee.dev@gmail.com>
2021-10-18 09:10:27 +0900

    test nestjs mvc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/')
+  @Render('login')
+  root(): { message: string } {
+    return { message: 'Hello world!!!' };
+  }
+
+  @Get('/home')
+  @Render('home')
+  getHome() {
+    return { user: 'test' };
   }
 }
